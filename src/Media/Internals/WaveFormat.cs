c0848281838832fd814ee78fdfcba2d18ba95ad9
Thismaker <smithkare47@gmail.com>
2023-03-03 22:10:23 +0300

    Add project files.

---
+++ src/Media/Internals/WaveFormat.cs
@@ -0,0 +1,28 @@
+﻿namespace Ocluse.LiquidSnow.Steganography.Media.Internals
+{
+    internal struct WaveFormat
+    {
+        public short AudioFormat { get; set; }
+
+        public short NumChannels { get; set; }
+
+        public int SampleRate { get; set; }
+
+        public short BitsPerSample { get; set; }
+
+        public int ByteRate
+        {
+            get { return SampleRate * NumChannels * BitsPerSample / 8; }
+        }
+
+        public int BytesPerSample
+        {
+            get { return BitsPerSample / 8; }
+        }
+
+        public short BlockAlign
+        {
+            get { return (short)(NumChannels * BitsPerSample / 8); }
+        }
+    }
+}
